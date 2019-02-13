aba497ccdf9f16fe06872caa2c0974d38014f6d1
hellbringer616 <m.johnsondelta@gmail.com>
2019-02-13 12:37:45 -0500

    itemcraft
    Added Mithril Ore and Mithril Ingots to itemizer and Harvester
    Through the Smelting config in to test that out.
    
    Need to decide if i want a 1:1 Itemizer/ByteItem for all crafting items or not.

--- config/nt-rpg/scripts/itempicture.js
+++ config/nt-rpg/scripts/itempicture.js
@@ -1,4 +1,4 @@
 var sponge = java.type("org.spongepowered.api.Sponge");
 var itemType = java.type("org.spongepowered.api.item.ItemType");
 
-skillService.getById("itemTd").get().setIcon(sponge.getRegistry().getType(itemType.static, "minecraft:nether_star").get());
+skillService.getById("itemId").get().setIcon(sponge.getRegistry().getType(itemType.static, "minecraft:nether_star").get());

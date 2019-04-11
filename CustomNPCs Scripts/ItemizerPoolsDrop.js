31c02fd357b6b8a9b34f28462a08682e61f147a4
hellbringer616 <m.johnsondelta@gmail.com>
2019-04-11 10:42:11 -0400

    Update ItemizerPoolsDrop.js
    Probably should make the NPC actually drop the item yeah? Yeah

--- CustomNPCs Scripts/ItemizerPoolsDrop.js
+++ CustomNPCs Scripts/ItemizerPoolsDrop.js
@@ -5,6 +5,8 @@
 var ItemService = Java.type("com.onaple.itemizer.ItemService");
 //Anything within the brackets runs on the death of an NPC
 function dies(event) {
-  //Fetches from the Itemizer pools.conf file using the numarical string ID
-  ItemService.fetch(1);
+  //Fetches from the Itemizer pools.conf file using the numarical string ID and sets it as the variable DroppedItem
+  var DroppedItem = ItemService.fetch(1);
+  //Sets the dropped item in slot 8 of CustomNPC to drop the itemizer item 100% of the time.
+  setDropedItem(int 8, IItemStack DroppedItem, int 100)
 }

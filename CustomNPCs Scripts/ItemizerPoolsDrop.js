704f16285da682a8cf3cbe348c32d1c8f4f92007
hellbringer616 <m.johnsondelta@gmail.com>
2019-04-11 08:13:04 -0400

    puttering
    Removed a work around to get Byte-Itrms to work (not needed as no longer using Byte Items)
    changed up a drop table to prepare for a script.
    
    Changed the Copper and Tin ore textures

--- CustomNPCs Scripts/ItemizerPoolsDrop.js
+++ CustomNPCs Scripts/ItemizerPoolsDrop.js
@@ -2,7 +2,7 @@
 //**NOTE** syntax for this will likely be all wrong; I need to keep better notes...
 
 //Loads a seperate API, in this case Itemizers ItemService
-var apiclass = Java.type("com.onaple.itemizer.ItemService");
+var ItemService = Java.type("com.onaple.itemizer.ItemService");
 //Anything within the brackets runs on the death of an NPC
 function dies(event) {
   //Fetches from the Itemizer pools.conf file using the numarical string ID

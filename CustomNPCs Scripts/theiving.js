75a56329831bbbb5b86441cbe27f9874a16f08f5
hellbringer616 <m.johnsondelta@gmail.com>
2019-05-20 12:57:03 -0400

    Update theiving.js
    Probably don't want to intialize values when they aren't needed.

--- CustomNPCs Scripts/theiving.js
+++ CustomNPCs Scripts/theiving.js
@@ -1,11 +1,11 @@
 //On interact with NPC
 function interact(event) {
-  var rng = Math.random();
-  var itemService = Java.type("com.onaple.itemizer.ItemService");
-  var playerName = event.source.getName();
-  var itemPool = ItemService.fetch(1);
-  
   if(event.source && event.source.getType() == EntityType_PLAYER && event.source.isSneaking()){
+    var rng = Math.random();
+    var itemService = Java.type("com.onaple.itemizer.ItemService");
+    var playerName = event.source.getName();
+    var itemPool = ItemService.fetch(1);
+
     if(rng >= 0.6){
       event.npc.executeCommand("fetch " itemPool + " 3 " playerName + "");
       event.npc.executeCommand("nadmin exp add " + playerName + " theiving " + maxHealth + "");

9a5c954452044d12aeaabbd82c477ad9baf6a908
hellbringer616 <m.johnsondelta@gmail.com>
2019-05-21 16:13:42 -0400

    Update theiving.js

--- CustomNPCs Scripts/theiving.js
+++ CustomNPCs Scripts/theiving.js
@@ -2,7 +2,7 @@
 function interact(event) {
   var sneak = event.player.isSneaking(); //is the player sneaking
 
-  if(sneak){ //checks if the player is sneaking
+  if(sneak && event.player.getPotionEffect(2) != 2){ //checks if the player is sneaking
     var rng = Math.random(); //generates a random number between 0 and 1
     var playerName = event.player.getName(); //gets the interacting players name
     if(rng >= 0.6){
@@ -10,7 +10,7 @@ function interact(event) {
     }else{
       event.npc.say("Beg your pardon?!");
       event.player.damage(2);
-      event.player.addPotionEffect(2, 3, 6, false); //adds slowness of 3 seconds of level 6 (immobile) to player
+      event.player.addPotionEffect(2, 3, 6, true); //adds slowness of 3 seconds of level 6 (immobile) to player
     }
   }else{
     event.npc.say("Hello Adventurer");

26c09d1876f22acd5497b35387223dd470585e74
hellbringer616 <m.johnsondelta@gmail.com>
2019-05-21 20:01:47 -0400

    stuff

--- CustomNPCs Scripts/thieving.js
+++ CustomNPCs Scripts/thieving.js
@@ -2,12 +2,12 @@
 function interact(event) {
   var sneak = event.player.isSneaking(); //is the player sneaking
 
-  if(sneak && event.player.getPotionEffect(2) != 2){ //checks if the player is sneaking
+  if(sneak && event.player.getPotionEffect(2) != 6){ //checks if the player is sneaking and if the player is not inflected with slowness strength 6
     var rng = Math.random(); //generates a random number between 0 and 1
     var playerName = event.player.getName(); //gets the interacting players name
     if(rng >= 0.6){
       event.npc.executeCommand("retrieve 9999 3 " + playerName + "") //retrieve itemizer item of quantity and give to player
-      event.npc.executeCommand("ndadmin exp add " + playerName + "thieving 8")
+      event.npc.executeCommand("nadmin exp add " + playerName + " thieving 8") //gives the player theiving XP
     }else{
       event.npc.say("Beg your pardon?!");
       event.player.damage(2);

7fc6277c63f70f1cefeafd7eef975a67450f0be9
hellbringer616 <m.johnsondelta@gmail.com>
2019-05-23 17:54:28 -0400

    scripting stuffs

--- CustomNPCs Scripts/thieving.js
+++ CustomNPCs Scripts/thieving.js
@@ -1,19 +1,32 @@
+//loads NTRPG Plugin
+var RpgPlugin = Java.type("cz.neumimto.rpg.NtRpgPlugin");
 //On interact with NPC
 function interact(event) {
   var sneak = event.player.isSneaking(); //is the player sneaking
+  var className = "Thieving" //must be a string
+  var uid = event.player.getUUID() //Get Player UUID string
+  var JavaUUID = Java.type('java.util.UUID') //Magic?
+  var character = RpgPlugin.GlobalScope.characterService.getCharacter(JavaUUID.fromString(uid)) //load NT-RPG Character service and get UUID than convert to Java UUID
+  var classData = character.getClasses().get(className) //gets class data from character
+  var requiredLevel = 1 //level required to steal
 
-  if(sneak && event.player.getPotionEffect(2) != 6){ //checks if the player is sneaking and if the player is not inflected with slowness strength 6
-    var rng = Math.random(); //generates a random number between 0 and 1
-    var playerName = event.player.getName(); //gets the interacting players name
-    if(rng >= 0.6){
-      event.npc.executeCommand("retrieve 9999 3 " + playerName + "") //retrieve itemizer item of quantity and give to player
-      event.npc.executeCommand("nadmin exp add " + playerName + " thieving 8") //gives the player theiving XP
+  if (classData !== null) { //checks if classData returns null
+    var classLevel = classData.getLevel()
+
+    if(sneak && event.player.getPotionEffect(2) != 6){ //checks if the player is sneaking and if the player is not inflected with slowness strength 6
+      var rng = Math.random(); //generates a random number between 0 and 1
+      var playerName = event.player.getName(); //gets the interacting players name
+      var roll = ((5/833)*((classLevel/requiredLevel)+(classLevel-requiredLevel)))+(17/49)+rng
+      if(roll >= 1){
+        event.npc.executeCommand("retrieve 9999 3 " + playerName + "") //retrieve itemizer item of quantity and give to player
+        event.npc.executeCommand("nadmin exp add " + playerName + " thieving 8") //gives the player theiving XP
+      }else{
+        event.npc.say("Beg your pardon?!");
+        event.player.damage(2);
+        event.player.addPotionEffect(2, 3, 6, true); //adds slowness of 3 seconds of level 6 (immobile) to player
+      }
     }else{
-      event.npc.say("Beg your pardon?!");
-      event.player.damage(2);
-      event.player.addPotionEffect(2, 3, 6, true); //adds slowness of 3 seconds of level 6 (immobile) to player
+      event.npc.say("Hello Adventurer");
     }
-  }else{
-    event.npc.say("Hello Adventurer");
   }
 }

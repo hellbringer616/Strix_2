19a77133e3d33a651a58d7c01be2389a3b6739aa
hellbringer616 <m.johnsondelta@gmail.com>
2019-08-04 18:53:17 -0400

    NT-RPG
    Update to 2.1.0 RC3- fixes to make compatible.

--- config/nt-rpg/scripts/itempicture.js
+++
@@ -1,4 +0,0 @@
-var sponge = Java.type("org.spongepowered.api.Sponge");
-var itemType = Java.type("org.spongepowered.api.item.ItemType");
-
-skillService.getById("itemId").get().setIcon(sponge.getRegistry().getType(itemType.static, "minecraft:nether_star").get());

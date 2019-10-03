4d4dad47373e5f589a61b377f67cee861bc4e827
hellbringer616 <m.johnsondelta@gmail.com>
2019-10-03 08:30:50 -0400

    Update combatlevel.js
    Maybe get this working?

--- config/placeholderapi/javascript/combatlevel.js
+++ config/placeholderapi/javascript/combatlevel.js
@@ -1,7 +1,9 @@
-var RpgPlugin = new JavaImporter("cz.neumimto.rpg.NtRpgPlugin")
-with(RpgPlugin) {
-    var combat = character.getClassByType("Combat");
+var RpgPlugin = new JavaImporter("cz.neumimto.rpg.NtRpgPlugin"); //load NT-RPG
+var JavaUUID = new JavaImporter('java.util.UUID'); //Magic?
+var uid = event.player.getUUID(); //Get Player UUID string
+var character = RpgPlugin.GlobalScope.characterService.getCharacter(JavaUUID.fromString(uid)); //load NT-RPG Character service and get UUID than convert to Java UUID
+var combat = character.getClassByType("Combat");
     if (combat !== null) {
        var level = combat.getLevel();
-    }
+
 }

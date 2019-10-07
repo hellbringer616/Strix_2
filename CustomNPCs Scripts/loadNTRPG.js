22935368aa0a79c05af16bd694a45d8ef1855346
hellbringer616 <m.johnsondelta@gmail.com>
2019-10-07 08:27:51 -0400

    update bronzecraft.conf
    Crafting set up for bronze, Exp needs to be balanced.

--- CustomNPCs Scripts/loadNTRPG.js
+++ CustomNPCs Scripts/loadNTRPG.js
@@ -9,7 +9,7 @@ function interact(event) {
   var JavaUUID = Java.type('java.util.UUID') //Magic?
   var character = RpgPlugin.GlobalScope.characterService.getCharacter(JavaUUID.fromString(uid)) //load NT-RPG Character service and get UUID than convert to Java UUID
   var classData = character.getClasses().get(className) //gets class data from character
-  if (classData !== null) { //checks if classData returns null
+  if(classData !== null) { //checks if classData returns null
     var classLevel = classData.getLevel()
     event.npc.say(classLevel)
   }

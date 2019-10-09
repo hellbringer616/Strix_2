abdaa84c5aac7f8ae9b230ef099e41543239d41e
hellbringer616 <m.johnsondelta@gmail.com>
2019-10-09 06:56:00 -0400

    Update weaponsmithlevel.js

--- config/placeholderapi/javascript/weaponsmithlevel.js
+++ config/placeholderapi/javascript/weaponsmithlevel.js
@@ -1,7 +1,7 @@
 //loads NTRPG Plugin
 var main = function() {
 var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin");
-  var className = "weaponsmith" //must be a string
+  var className = "Weaponsmith" //must be a string
   var uid = player.getUniqueId(); //Get Player UUID
   var character = RpgPlugin.GlobalScope.characterService.getCharacter(uid); //load NT-RPG Character service and get UUID for player
   var classData = character.getClasses().get(className); //gets class data from character

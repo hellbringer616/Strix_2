48c9215a6b359f297ed7610d99179d5f64be6a49
hellbringer616 <m.johnsondelta@gmail.com>
2019-10-09 06:51:36 -0400

    levels
    Display level in menus (Thank you GlassSpirit for help debugging!)

--- config/placeholderapi/javascript/combatlevel.js
+++ config/placeholderapi/javascript/combatlevel.js
@@ -1,10 +1,14 @@
 //loads NTRPG Plugin
+var main = function() {
 var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin");
   var className = "Combat" //must be a string
-  var uid = player.getUUID(); //Get Player UUID string
-  var JavaUUID = Java.type('java.util.UUID'); //problematic line...
-  var character = RpgPlugin.GlobalScope.characterService.getCharacter(JavaUUID.fromString(uid)); //load NT-RPG Character service and get UUID than convert to Java UUID
+  var uid = player.getUniqueId(); //Get Player UUID
+  var character = RpgPlugin.GlobalScope.characterService.getCharacter(uid); //load NT-RPG Character service and get UUID for player
   var classData = character.getClasses().get(className); //gets class data from character
   if(classData !== null) { //checks if classData returns null
     var out = classData.getLevel();
+
+	return out;
   }
+}
+main();
--- config/placeholderapi/javascript/metallurgylevel.js
+++ config/placeholderapi/javascript/metallurgylevel.js
@@ -1,7 +1,14 @@
-var RpgPlugin = new JavaImporter("cz.neumimto.rpg.NtRpgPlugin");
-with(RpgPlugin) {
-    var metallurgy = character.getClassByType("Metallurgy");
-    if (metallurgy !== null) {
-       var out = metallurgy.getLevel();
-    }
+//loads NTRPG Plugin
+var main = function() {
+var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin");
+  var className = "Metallurgy" //must be a string
+  var uid = player.getUniqueId(); //Get Player UUID
+  var character = RpgPlugin.GlobalScope.characterService.getCharacter(uid); //load NT-RPG Character service and get UUID for player
+  var classData = character.getClasses().get(className); //gets class data from character
+  if(classData !== null) { //checks if classData returns null
+    var out = classData.getLevel();
+
+	return out;
+  }
 }
+main();

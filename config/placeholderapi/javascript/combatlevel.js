c7aaba8f15ab2ddab95bf613dc78c35bfdda5edc
hellbringer616 <m.johnsondelta@gmail.com>
2019-10-09 06:30:41 -0400

    Update combatlevel.js
    still broken

--- config/placeholderapi/javascript/combatlevel.js
+++ config/placeholderapi/javascript/combatlevel.js
@@ -1,7 +1,7 @@
 //loads NTRPG Plugin
 var RpgPlugin = Java.type("cz.neumimto.rpg.sponge.NtRpgPlugin");
   var className = "Combat" //must be a string
-  var uid = event.player.getUUID(); //Get Player UUID string
+  var uid = player.getUUID(); //Get Player UUID string
   var JavaUUID = Java.type('java.util.UUID'); //problematic line...
   var character = RpgPlugin.GlobalScope.characterService.getCharacter(JavaUUID.fromString(uid)); //load NT-RPG Character service and get UUID than convert to Java UUID
   var classData = character.getClasses().get(className); //gets class data from character
